56f21ec4f4c5ebf7ef5500846781c59c2e3e4278
Juyoung Lee <ljy070100@gmail.com>
2020-06-21 18:17:03 +0900

    닫기 버튼을 누르면 실제로 화면을 종료함

--- app/src/main/java/kr/co/namu/project_20200621/FirstActivity2.kt
+++ app/src/main/java/kr/co/namu/project_20200621/FirstActivity2.kt
@@ -1,11 +1,25 @@
 package kr.co.namu.project_20200621
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first2.*
 
 class FirstActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first2)
+
+
+
+
+
+        closeBtn.setOnClickListener {
+
+            finish()
+//            val myIntent = Intent(this, MainActivity::class.java)
+
+//            startActivity(myIntent)
+        }
     }
 }
