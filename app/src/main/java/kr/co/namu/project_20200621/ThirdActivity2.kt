694ba079b32ff8fc8f07765820b97dae8dfdb20d
Juyoung Lee <ljy070100@gmail.com>
2020-06-21 19:04:46 +0900

    세번째 activity, 첨부된 메세지를 받아서 textview에 반영

--- app/src/main/java/kr/co/namu/project_20200621/ThirdActivity2.kt
+++ app/src/main/java/kr/co/namu/project_20200621/ThirdActivity2.kt
@@ -2,10 +2,19 @@ package kr.co.namu.project_20200621
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_third2.*
 
 class ThirdActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third2)
+
+//        들어올때 message 라는 이름이 적힌 String 있는지 확인 => 결과를 변수에 저
+        val receivedMessage = intent.getStringExtra("message")
+
+        messageTxt.text = receivedMessage
     }
+
+
+
 }
